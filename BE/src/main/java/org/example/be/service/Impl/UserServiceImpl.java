1390bb280f9439e737b60428765d8d3adf47d045
lui <hswn4237@naver.com>
2025-03-03 23:55:37 +0900

    move /login to UserController

--- BE/src/main/java/org/example/be/service/Impl/UserServiceImpl.java
+++ BE/src/main/java/org/example/be/service/Impl/UserServiceImpl.java
@@ -4,10 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.example.be.model.User;
 import org.example.be.repository.UserRepository;
 import org.example.be.security.dto.JoinRequestDto;
+import org.example.be.security.dto.LoginRequestDto;
+import org.example.be.security.jwt.JwtUtil;
 import org.example.be.service.UserService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +24,8 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
     @Override
     public String join(JoinRequestDto request) {
@@ -36,4 +47,17 @@ public class UserServiceImpl implements UserService {
         return "가입을 환영합니다 !";
     }
 
+    @Override
+    public Map<String, String> login(LoginRequestDto request) {
+        Optional<User> user = userRepository.findByEmail(request.getEmail());
+        if (!user.isPresent() || !passwordEncoder.matches(request.getPassword(), user.get().getPassword())) {
+            return Map.of("error", "Invalid email or password");
+        }
+
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.get().getUsername(), request.getPassword())
+        );
+        String token = jwtUtil.generateToken(user.get().getUsername());
+        return Map.of("token", token);
+    }
 }
--- BE/src/main/java/org/example/be/service/UserService.java
+++ BE/src/main/java/org/example/be/service/UserService.java
@@ -1,8 +1,12 @@
 package org.example.be.service;
 
 import org.example.be.security.dto.JoinRequestDto;
+import org.example.be.security.dto.LoginRequestDto;
+
+import java.util.Map;
 
 public interface UserService {
 
     String join(JoinRequestDto request);
+    Map<String, String> login(LoginRequestDto request);
 }
